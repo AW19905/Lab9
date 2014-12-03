41edd41d797d17422c5d3c69355ea3e270afe315
AW19905 <aniawestphal1@wp.pl>
2014-12-03 13:11:40 +0100

    Laboratorium 9 - sklep projekt

---
+++ Model/MovieRent.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class MovieRent extends Movie {
+
+    /**
+     * 
+     */
+    public MovieRent() {
+    }
+
+    /**
+     * 
+     */
+    public void Stato {in, out};
+
+    /**
+     * 
+     */
+    public void data prestito;
+
+    /**
+     * 
+     */
+    public void Prezzo;
+
+}
---
+++ Model/MovieShop.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class MovieShop {
+
+    /**
+     * 
+     */
+    public MovieShop() {
+    }
+
+    /**
+     * 
+     */
+    public void Attribute1;
+
+    /**
+     * 
+     */
+    public void subscribe;
+
+
+
+
+}
